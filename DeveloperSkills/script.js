d6f22c8b381001fe5963def0876fe58324242343
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-04-07 04:06:48 +1000

    few more challenges

--- DeveloperSkills/script.js
+++ DeveloperSkills/script.js
@@ -65,3 +65,58 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // };
 // const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 // console.log(amplitudeNew);
+
+// Debugging with the Console and Breakpoints
+
+// const Kelvinmeasure = function () {
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'celsius',
+//     value: Number(prompt('Degrees Celsius:')), // prompt function always returns string
+//   };
+//   // console.log(measurement);
+//   // console.table(measurement);
+//   // console.log(measurement.value);
+//   // console.warn(measurement.value);
+//   // console.error(measurement.value);
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+// console.log(Kelvinmeasure());
+
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+// 1. Understanding problem:
+// Array transformed to string seperated by ...;
+// Whati sthedays: array index +1
+
+// 2. Breaking into sub problems
+// Transformation of arrays into strings
+// Strings needs to contain day (index+1);
+// Add ... between elements and start and end of string
+// const data1 = [17, 21, 23];
+// const data2 = [12, 5, -5, 0, 4];
+
+// // console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+// const printForecast = (arr) => {
+//   let str = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     str = str + `${arr[i]}C in ${i + 1} days ...`;
+//   }
+//   console.log('...' + str);
+// };
+// printForecast(data1);
